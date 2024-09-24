3ed79ab16be5df608034cfb82ced67120736eee9
DevKDuck <loading95@naver.com>
2024-09-24 11:43:10 +0900

    TodoDAO 목록 기능 구현 + 테스트

--- src/main/java/com/devduck/duckmovie/dao/TodoDAO.java
+++ src/main/java/com/devduck/duckmovie/dao/TodoDAO.java
@@ -1,10 +1,14 @@
 package com.devduck.duckmovie.dao;
 
+import com.devduck.duckmovie.domain.TodoVO;
 import lombok.Cleanup;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class TodoDAO {
@@ -36,5 +40,37 @@ public class TodoDAO {
         String now = resultSet.getString(1);
 
         return now;
+    }
+
+    public void insert(TodoVO todoVO) throws Exception{
+        String sql = "insert into tbl_todo (title,dueDate,finished) values (?,?,?)";
+
+        @Cleanup PreparedStatement preparedStatement = ConnectionUtil.INSTANCE.getConnection().prepareStatement(sql);
+
+        preparedStatement.setString(1, todoVO.getTitle());
+        preparedStatement.setDate(2, Date.valueOf(todoVO.getDueDate()));
+        preparedStatement.setBoolean(3, todoVO.isFinished());
+
+        preparedStatement.executeUpdate();
+    }
+
+    public List<TodoVO> selectAll() throws Exception{
+        String sql = "select * from tbl_todo";
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+        List<TodoVO> list = new ArrayList<>();
+        while(resultSet.next()){
+            TodoVO vo = TodoVO.builder()
+                    .tno(resultSet.getLong("tno"))
+                    .title(resultSet.getString("title"))
+                    .dueDate(resultSet.getDate("dueDate").toLocalDate())
+                    .finished(resultSet.getBoolean("finished"))
+                    .build();
+
+            list.add(vo);
+        }
+        return list;
     }
 }
--- src/test/java/TodoDAOTest.java
+++ src/test/java/TodoDAOTest.java
@@ -1,6 +1,10 @@
 import com.devduck.duckmovie.dao.TodoDAO;
+import com.devduck.duckmovie.domain.TodoVO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class TodoDAOTest {
     private TodoDAO todoDAO;
@@ -13,5 +17,18 @@ public class TodoDAOTest {
     @Test
     public void testTime() throws Exception{
         System.out.println(todoDAO.getTime());
+    }
+
+    @Test
+    public void testInsert() throws Exception{
+        TodoVO todoVO = TodoVO.builder().title("Sample Title...").dueDate(LocalDate.of(2021,12,31)).build();
+
+        todoDAO.insert(todoVO);
+    }
+
+    @Test
+    public void testList() throws Exception{
+        List<TodoVO> list = todoDAO.selectAll();
+        list.forEach(System.out::println);
     }
 }
