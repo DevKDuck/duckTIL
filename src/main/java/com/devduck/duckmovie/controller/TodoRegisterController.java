bdc6cc8b5bebba76be9ca248186a88b321660010
DevKDuck <loading95@naver.com>
2024-09-25 13:14:03 +0900

    JSESSIONID 테스트

--- src/main/java/com/devduck/duckmovie/controller/TodoRegisterController.java
+++ src/main/java/com/devduck/duckmovie/controller/TodoRegisterController.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -23,6 +24,23 @@ public class TodoRegisterController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.info("/todo/register GET........");
+
+        HttpSession session = req.getSession();
+
+        //기존에 JSEESIONID가 없는 새로운 사용자
+        if(session.isNew()){
+            log.info("JSESSIONID 쿠키가 새로 만들어진 사용자입니다.");
+            resp.sendRedirect("/login");
+            return;
+        }
+
+        //JSESSIONID가 있지만 세션 컨텍스트에 logInfo라는 이름으로 저장된 객체가 없는 경우
+        if(session.getAttribute("logInfo") == null){
+            log.info("로그인한 정보가 없는 사용자");
+            resp.sendRedirect("/login");
+            return;
+        }
+
         req.getRequestDispatcher("/WEB-INF/todo/register.jsp").forward(req, resp);
     }
 
