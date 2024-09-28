e349c14ea4a65e59311b06ed8dbbc988f2619ca3
DevKDuck <loading95@naver.com>
2024-09-28 23:32:03 +0900

    component scan test

--- src/main/java/com/devduck/duckmovie/sample/SampleDAO.java
+++ src/main/java/com/devduck/duckmovie/sample/SampleDAO.java
@@ -1,4 +1,7 @@
 package com.devduck.duckmovie.sample;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class SampleDAO {
 }
--- src/main/java/com/devduck/duckmovie/sample/SampleService.java
+++ src/main/java/com/devduck/duckmovie/sample/SampleService.java
@@ -1,4 +1,13 @@
 package com.devduck.duckmovie.sample;
 
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@ToString
+@Service
 public class SampleService {
+    @Autowired
+    private SampleDAO sampleDAO;
+
 }
